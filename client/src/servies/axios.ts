4f649dca4d41983ed43fc8b78a380ae47f0d64d4
ssb-shree <bavachikarshree1666@gmail.com>
2025-09-01 13:30:09 +0530

    add url

--- client/src/servies/axios.ts
+++ client/src/servies/axios.ts
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  // baseURL: process.env.NEXT_PUBLIC_STATUS === "DEV" ? "http://localhost:8080" : process.env.NEXT_PUBLIC_BACKENDURL,
-  baseURL: "http://localhost:8080",
+  baseURL: process.env.NEXT_PUBLIC_STATUS === "DEV" ? "http://localhost:8080" : process.env.NEXT_PUBLIC_BACKENDURL,
 });
 
 export default axiosInstance;
