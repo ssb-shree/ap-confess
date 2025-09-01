45f32f14f67d309ef81c6054b055252c5fddffb9
ssb-shree <bavachikarshree1666@gmail.com>
2025-09-01 22:02:13 +0530

    minimum category is now 3

--- server/src/controllers/confessions.schema.ts
+++ server/src/controllers/confessions.schema.ts
@@ -3,7 +3,7 @@ import z from "zod";
 const writeSchema = z.object({
   title: z.string().min(3).max(250),
   body: z.string().min(25),
-  categories: z.array(z.string().min(2).max(20)).min(5).max(20),
+  categories: z.array(z.string().min(2).max(20)).min(3).max(20),
 });
 
 const mongoID = z.object({ ID: z.string().min(1).max(25) });
