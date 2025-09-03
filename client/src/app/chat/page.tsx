53f611cf103dda23691e82be47e3fc4d080d85f2
ssb-shree <bavachikarshree1666@gmail.com>
2025-09-03 12:44:59 +0530

    Added chat page

---
+++ client/src/app/chat/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Chatpage = () => {
+  return (
+    <div className="w-screen min-h-screen flex justify-center items-center text-2xl md:text-4xl">
+      Under Construction
+    </div>
+  );
+};
+
+export default Chatpage;
